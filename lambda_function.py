9d4929a0173ebae5ba46dc06afc5394543d63a00
ianfer19 <ifernandezalvarez@correo.unicordoba.edu.co>
2025-11-20 00:55:13 -0500

    Implementar creación de proyectos en lambda_function.py con manejo de errores y respuestas HTTP

--- lambda_function.py
+++ lambda_function.py
@@ -1,5 +1,19 @@
+import json
+from services.project_service import ProjectService
+from utils.http_responses import success, error
+
 def lambda_handler(event, context):
-    return {
-        "statusCode": 200,
-        "body": "Hola desde Lambda desplegada con GitHub Actions 🎉 Prueba"
-    }
+    try:
+        if "body" not in event:
+            return error("Body vacío")
+
+        body = json.loads(event["body"])
+
+        created = ProjectService.create_project(body)
+        return success({
+            "message": "Proyecto creado correctamente",
+            "project": created
+        }, 201)
+
+    except Exception as e:
+        return error(str(e), 400)
